25787149e3813edb5490eb43b9e10fa68cc64bc1
Julia Lewis-Borgia <jlewisborgia@smith.edu>
2023-02-15 23:04:17 -0500

    first commit

--- Conversation.java
+++ Conversation.java
@@ -1,6 +1,84 @@
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ * this is the class for the Chatbot conversation
+ */
 class Conversation {
 
+  public Conversation() {
+  }
+  
+/**
+ * runs chatbot by asking how many rounds, taking in user inputs, outputting randomized responses, changing pronouns, and storting and printing transcript
+ * @param arguments 
+ */
   public static void main(String[] arguments) {
-    // You will start the conversation here.
+    Scanner rounds = new Scanner(System.in);
+    System.out.println("How many rounds? ");
+    int x = rounds.nextInt();
+    String[] transcript = new String[2*(x) + 2 ];
+    transcript[0] = "Hi there!  What's on your mind?"; 
+    System.out.println("Hi there!  What's on your mind?");
+    String[] responses = new String[] { "Interesting", "Mhmm...", "That's cool.", "I agree", "Uh-huh", "Really?"};
+
+    /**
+     * replaced pronouns when necessary and otherwise produces random responses
+     */
+    for (int i = 0; i < x; i++){
+      Scanner converse = new Scanner(System.in);
+      String user = converse.nextLine();
+      transcript[2*i+1] = user;
+      String mirrored;
+      String new_user = user.replace("I", "I");
+      new_user = new_user.replace("me ","2you2 ");
+      new_user = new_user.replace(" me"," 2you2");
+      new_user = new_user.replace("my ","1your1 ");
+      new_user = new_user.replace(" my "," 1your1");
+      new_user = new_user.replace("am ","1are1 ");
+      new_user = new_user.replace(" am"," 1are1");
+      new_user = new_user.replace("your ","1my1 ");
+      new_user = new_user.replace(" your "," 1my1");
+      new_user = new_user.replace("you ","1I1 ");
+      new_user = new_user.replace(" you"," 1I1");
+      new_user = new_user.replace("are ","1am1 ");
+      new_user = new_user.replace(" are"," 1am1");
+      new_user = new_user.replace("I ", "1you1 ");
+      new_user = new_user.replace(" I", " 1you1");
+
+      
+      new_user = new_user.replace("1am1","am");
+      new_user = new_user.replace("1you1","you");
+      new_user = new_user.replace("2you2","you");
+      new_user = new_user.replace("1your1","your");
+      new_user = new_user.replace("1are1","are");
+      new_user = new_user.replace("1my1","my");
+      new_user = new_user.replace("1I1","I");
+      new_user = new_user.replace("1I2","I");
+
+      
+      if (!new_user.equals(user)) {
+        mirrored = new_user;
+      }
+      else{
+        Random random = new Random();
+        mirrored = responses[random.nextInt(responses.length)];
+      }
+      transcript[2*i+2] = mirrored;
+      System.out.println(mirrored);
+            
+    }
+      /**
+       * finishes and prints out transcript for user
+       */
+      transcript[2*(x) + 1] = "See ya!";
+      System.out.println("See ya!");
+      Arrays.stream(transcript).forEach(System.out::println);
+      System.out.println(" ");
+      System.out.println("TRANSCRIPT: ");
+      System.out.println(Arrays.deepToString(transcript));
   }
 }
+
+
--- reflection.md
+++ reflection.md
@@ -1,7 +1,9 @@
 Use this file to record your reflection on this assignment.
 
-- Which classmates did you consult with while working on this assignment?
-- Which session(s) of TA / office hours did you attend?
-- What are your initial impressions of Java? 
+- Which classmates did you consult with while working on this assignment? I worked on my own for this assignment.
+- Which session(s) of TA / office hours did you attend? I went to TA hours on Wednesday night.
+- What are your initial impressions of Java? My initial impressions of Java was that some of it reminded me a lot of Python but there are also some things that I have to just get used to adjusting my mindset on.
 - Can you draw any conclusion about programming in general from the similarities or the differences between the two languages? 
+    Alot of the literal code is similar in its structure but Java being object-oriented means you have to approach each project with more planning.
 - What worked, what didn't, what advice would you give someone taking this course in the future?
+    I wanted to use more methods and a better process for changing pronouns. Go to TA hours if you can!
